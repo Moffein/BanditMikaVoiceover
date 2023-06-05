2ca0f7d709afe4ff4f36fdc006d03e97562bda61
Moffein <22742925+Moffein@users.noreply.github.com>
2023-06-04 18:52:13 -0700

    Add project files.

---
+++ BanditMikaVoiceover/BanditMikaVoiceoverPlugin.cs
@@ -0,0 +1,185 @@
+﻿using BanditMikaVoiceover.Components;
+using BepInEx;
+using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using RoR2.Audio;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+
+namespace BanditMikaVoiceover
+{
+    [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Foxyz.BanditMika")]
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Schale.BanditMikaVoiceover", "BanditMikaVoiceover", "0.1.0")]
+    [R2API.Utils.R2APISubmoduleDependency(nameof(SoundAPI), nameof(ContentAddition))]
+    public class BanditMikaVoiceoverPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> enableVoicelines;
+        private static SurvivorDef banditSurvivorDef = Addressables.LoadAssetAsync<SurvivorDef>("RoR2/Base/Bandit2/Bandit2.asset").WaitForCompletion();
+        public static bool playedSeasonalVoiceline = false;
+        public static AssetBundle assetBundle;
+
+        public void Awake()
+        {
+            BaseVoiceoverComponent.Init();
+            RoR2.RoR2Application.onLoad += OnLoad;
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BanditMikaVoiceover.banditmikavoiceoverbundle"))
+            {
+                assetBundle = AssetBundle.LoadFromStream(stream);
+            }
+
+            using (var bankStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BanditMikaVoiceover.BanditMikaSoundbank.bnk"))
+            {
+                var bytes = new byte[bankStream.Length];
+                bankStream.Read(bytes, 0, bytes.Length);
+                R2API.SoundAPI.SoundBanks.Add(bytes);
+            }
+
+            InitNSE();
+
+            enableVoicelines = base.Config.Bind<bool>(new ConfigDefinition("Settings", "Enable Voicelines"), true, new ConfigDescription("Enable voicelines when using the Bandit Mika Skin."));
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+            {
+                RiskOfOptionsCompat();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void RiskOfOptionsCompat()
+        {
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableVoicelines));
+            RiskOfOptions.ModSettingsManager.SetModIcon(assetBundle.LoadAsset<Sprite>("texModIcon"));
+        }
+
+        private void OnLoad()
+        {
+            bool foundSkin = false;
+            SkinDef[] commandoSkins = SkinCatalog.FindSkinsForBody(BodyCatalog.FindBodyIndex("CommandoBody"));
+            foreach (SkinDef skinDef in commandoSkins)
+            {
+                //nameToken is "ALICKET_SKIN_BAHYCSKINDEFINITION_NAME"
+                if (skinDef.name == "BanditMikaDef")
+                {
+                    foundSkin = true;
+                    BanditMikaVoiceoverComponent.requiredSkinDefs.Add(skinDef);
+                    break;
+                }
+            }
+
+            if (!foundSkin)
+            {
+                Debug.LogError("BanditMikaVoiceover: Bandit Mika SkinDef not found. Voicelines will not work!");
+            }
+            else
+            {
+                On.RoR2.CharacterBody.Start += AttachVoiceoverComponent;
+
+                On.RoR2.SurvivorMannequins.SurvivorMannequinSlotController.RebuildMannequinInstance += (orig, self) =>
+                {
+                    orig(self);
+                    if (self.currentSurvivorDef == banditSurvivorDef)
+                    {
+                        //Loadout isn't loaded first time this is called, so we need to manually get it.
+                        //Probably not the most elegant way to resolve this.
+                        if (self.loadoutDirty)
+                        {
+                            if (self.networkUser)
+                            {
+                                self.networkUser.networkLoadout.CopyLoadout(self.currentLoadout);
+                            }
+                        }
+
+                        //Check SkinDef
+                        BodyIndex bodyIndexFromSurvivorIndex = SurvivorCatalog.GetBodyIndexFromSurvivorIndex(self.currentSurvivorDef.survivorIndex);
+                        int skinIndex = (int)self.currentLoadout.bodyLoadoutManager.GetSkinIndex(bodyIndexFromSurvivorIndex);
+                        SkinDef safe = HG.ArrayUtils.GetSafe<SkinDef>(BodyCatalog.GetBodySkins(bodyIndexFromSurvivorIndex), skinIndex);
+                        if (BanditMikaVoiceoverComponent.requiredSkinDefs.Contains(safe))
+                        {
+                            bool played = false;
+                            if (!playedSeasonalVoiceline)
+                            {
+                                if (System.DateTime.Today.Month == 1 && System.DateTime.Today.Day == 1)
+                                {
+                                    Util.PlaySound("Play_BanditMika_Lobby_Newyear", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 5 && System.DateTime.Today.Day == 8)
+                                {
+                                    Util.PlaySound("Play_BanditMika_Lobby_bday", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 10 && System.DateTime.Today.Day == 31)
+                                {
+                                    Util.PlaySound("Play_BanditMika_Lobby_Halloween", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+                                else if (System.DateTime.Today.Month == 12 && System.DateTime.Today.Day == 25)
+                                {
+                                    Util.PlaySound("Play_BanditMika_Lobby_xmas", self.mannequinInstanceTransform.gameObject);
+                                    played = true;
+                                }
+
+                                if (played)
+                                {
+                                    playedSeasonalVoiceline = true;
+                                }
+                            }
+                            if (!played)
+                            {
+                                if (Util.CheckRoll(5f))
+                                {
+                                    Util.PlaySound("Play_BanditMika_TitleDrop", self.mannequinInstanceTransform.gameObject);
+                                }
+                                else
+                                {
+                                    Util.PlaySound("Play_BanditMika_Lobby", self.mannequinInstanceTransform.gameObject);
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+            BanditMikaVoiceoverComponent.ScepterIndex = ItemCatalog.FindItemIndex("ITEM_ANCIENT_SCEPTER");
+        }
+
+        private void InitNSE()
+        {
+            BanditMikaVoiceoverComponent.nseBlock = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditMikaVoiceoverComponent.nseBlock.eventName = "Play_BanditMika_Blocked";
+            R2API.ContentAddition.AddNetworkSoundEventDef(BanditMikaVoiceoverComponent.nseBlock);
+
+            BanditMikaVoiceoverComponent.nseShrineFail = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditMikaVoiceoverComponent.nseShrineFail.eventName = "Play_BanditMika_ShrineFail";
+            R2API.ContentAddition.AddNetworkSoundEventDef(BanditMikaVoiceoverComponent.nseShrineFail);
+
+            BanditMikaVoiceoverComponent.nseShout = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditMikaVoiceoverComponent.nseShout.eventName = "Play_BanditMika_Shout";
+            R2API.ContentAddition.AddNetworkSoundEventDef(BanditMikaVoiceoverComponent.nseShout);
+
+            BanditMikaVoiceoverComponent.nseSpecial = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            BanditMikaVoiceoverComponent.nseSpecial.eventName = "Play_BanditMika_TacticalAction";
+            R2API.ContentAddition.AddNetworkSoundEventDef(BanditMikaVoiceoverComponent.nseSpecial);
+        }
+
+
+        private void AttachVoiceoverComponent(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            if (self)
+            {
+                if (BanditMikaVoiceoverComponent.requiredSkinDefs.Contains(SkinCatalog.GetBodySkinDef(self.bodyIndex, (int)self.skinIndex)))
+                {
+                    BaseVoiceoverComponent existingVoiceoverComponent = self.GetComponent<BaseVoiceoverComponent>();
+                    if (!existingVoiceoverComponent) self.gameObject.AddComponent<BanditMikaVoiceoverComponent>();
+                }
+            }
+        }
+    }
+}
---
+++ BanditMikaVoiceover/Components/BanditMikaVoiceoverComponent.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BanditMikaVoiceover.Components
+{
+    public class BanditMikaVoiceoverComponent : BaseVoiceoverComponent
+    {
+        public static List<SkinDef> requiredSkinDefs = new List<SkinDef>();
+        public static NetworkSoundEventDef nseBlock;
+        public static NetworkSoundEventDef nseShrineFail;
+        public static NetworkSoundEventDef nseShout;
+        public static NetworkSoundEventDef nseSpecial;
+
+        public static ItemIndex ScepterIndex;
+
+        private float blockedCooldown = 0f;
+        private float levelCooldown = 0f;
+        private float shrineOfChanceFailCooldown = 0f;
+        private float specialCooldown = 0f;
+        private bool acquiredScepter = false;
+
+        protected override void Awake()
+        {
+            spawnVoicelineDelay = 3f;
+            if (Run.instance && Run.instance.stageClearCount == 0)
+            {
+                spawnVoicelineDelay = 6.5f;
+            }
+            base.Awake();
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (blockedCooldown > 0f) blockedCooldown -= Time.fixedDeltaTime;
+            if (levelCooldown > 0f) levelCooldown -= Time.fixedDeltaTime;
+            if (shrineOfChanceFailCooldown > 0f) shrineOfChanceFailCooldown -= Time.fixedDeltaTime;
+            if (specialCooldown > 0f) specialCooldown -= Time.fixedDeltaTime;
+        }
+
+
+        public override void PlayDamageBlockedServer()
+        {
+            if (!NetworkServer.active || blockedCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseBlock, 2.2f, false);
+            if (played) blockedCooldown = 10f;
+        }
+
+        public override void PlayDeath()
+        {
+            TryPlaySound("Play_BanditMika_Defeat", 4.5f, true);
+        }
+
+        public override void PlayHurt(float percentHPLost)
+        {
+            if (percentHPLost >= 0.1f)
+            {
+                TryPlaySound("Play_BanditMika_TakeDamage", 0f, false);
+            }
+        }
+
+        public override void PlayJump() { }
+
+        public override void PlayLevelUp()
+        {
+            if (levelCooldown > 0f) return;
+            bool played = TryPlaySound("Play_BanditMika_LevelUp", 10f, false);
+            if (played) levelCooldown = 60f;
+        }
+
+        public override void PlayLowHealth() { }
+
+        public override void PlayPrimaryAuthority() { }
+
+        public override void PlaySecondaryAuthority()
+        {
+            TryPlayNetworkSound(nseShout, 0f, false);
+        }
+
+        public override void PlaySpawn()
+        {
+            TryPlaySound("Play_BanditMika_Spawn", 3f, true);
+        }
+
+        public override void PlaySpecialAuthority()
+        {
+            if (specialCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseSpecial, 1.9f, false);
+            if (played) specialCooldown = 30f;
+        }
+
+        public override void PlayTeleporterFinish()
+        {
+            TryPlaySound("Play_BanditMika_Victory", 4f, false);
+        }
+
+        public override void PlayTeleporterStart()
+        {
+            TryPlaySound("Play_BanditMika_TeleporterStart", 4.2f, false);
+        }
+
+        public override void PlayUtilityAuthority() { }
+
+        public override void PlayVictory()
+        {
+            TryPlaySound("Play_BanditMika_Victory", 4f, true);
+        }
+
+        protected override void Inventory_onItemAddedClient(ItemIndex itemIndex)
+        {
+            base.Inventory_onItemAddedClient(itemIndex);
+            if (BanditMikaVoiceoverComponent.ScepterIndex != ItemIndex.None && itemIndex == BanditMikaVoiceoverComponent.ScepterIndex)
+            {
+                PlayAcquireScepter();
+            }
+            else
+            {
+                ItemDef id = ItemCatalog.GetItemDef(itemIndex);
+                if (itemIndex == RoR2Content.Items.Squid.itemIndex || itemIndex == RoR2Content.Items.Plant.itemIndex)
+                {
+                    PlayBadItem();
+                }
+                else if (id && id.deprecatedTier == ItemTier.Tier3)
+                {
+                    PlayAcquireLegendary();
+                }
+            }
+        }
+
+        public void PlayAcquireScepter()
+        {
+            if (acquiredScepter) return;
+            TryPlaySound("Play_BanditMika_AcquireScepter", 19f, false);
+            acquiredScepter = true;
+        }
+
+        public void PlayAcquireLegendary()
+        {
+            TryPlaySound("Play_BanditMika_FindLegendary", 9f, false);
+        }
+
+        public void PlayBadItem()
+        {
+            TryPlaySound("Play_BanditMika_BadItem", 4.2f, false);
+        }
+    }
+}
